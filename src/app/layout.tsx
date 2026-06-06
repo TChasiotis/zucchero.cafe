2a132b58b4c75740846f1fb4979e0f943b4c9127
Thomas <thomaschasiotis05@gmail.com>
2026-06-06 15:07:02 +0300

    -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,11 @@ export default function RootLayout({
       lang="el"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        {/* Εδώ βάζεις το πραγματικό σου ID όταν το πάρεις από τη Google */}
+        <GoogleAnalytics gaId="G-SFV5CELQTL" />
+      </body>
     </html>
   );
 }
